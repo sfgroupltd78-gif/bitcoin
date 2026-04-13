0d392fe88eab1734265dff488ce12d61f4868b88
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-13 12:11:54 +0800

    Update freebsd.mk

--- depends/builders/freebsd.mk
+++ depends/builders/freebsd.mk
@@ -7,3 +7,4 @@ build_freebsd_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CON
 # freebsd host on freebsd builder: override freebsd host preferences.
 freebsd_CC = clang
 freebsd_CXX = clang++
+ 
