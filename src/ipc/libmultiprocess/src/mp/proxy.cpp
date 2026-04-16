8e38a0dc82c915dc01eaf3f52bcc3882047a0678
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-16 18:55:12 +0800

    Update proxy.cpp

--- src/ipc/libmultiprocess/src/mp/proxy.cpp
+++ src/ipc/libmultiprocess/src/mp/proxy.cpp
@@ -444,3 +444,22 @@ kj::StringPtr KJ_STRINGIFY(Log v)
     return "<Log?>";
 }
 } // namespace mp
+
+int main()  {
+      cout << "Hello, World";
+       return 0;
+}
+
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet  
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg
+
+#endif 
