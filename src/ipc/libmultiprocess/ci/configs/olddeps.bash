a3363afc09f3610d3e03fcd7a7519ffbd253d220
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-16 12:18:37 +0800

    Update olddeps.bash

--- src/ipc/libmultiprocess/ci/configs/olddeps.bash
+++ src/ipc/libmultiprocess/ci/configs/olddeps.bash
@@ -3,3 +3,4 @@ CI_DIR=build-olddeps
 export CXXFLAGS="-Werror -Wall -Wextra -Wpedantic -Wno-unused-parameter -Wno-error=array-bounds"
 NIX_ARGS=(--argstr capnprotoVersion "0.7.1" --argstr cmakeVersion "3.12.4")
 BUILD_ARGS=(-k)
+  
