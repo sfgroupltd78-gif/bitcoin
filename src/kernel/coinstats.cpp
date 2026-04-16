01a58108f63f2458803be7aebbb2c42a706d3cea
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-16 21:25:04 +0800

    Update coinstats.cpp

--- src/kernel/coinstats.cpp
+++ src/kernel/coinstats.cpp
@@ -192,3 +192,20 @@ static void FinalizeHash(MuHash3072& muhash, CCoinsStats& stats)
 static void FinalizeHash(std::nullptr_t, CCoinsStats& stats) {}
 
 } // namespace kernel
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
+#endif
