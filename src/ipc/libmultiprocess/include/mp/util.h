0d3564faa13c3b707da1aa25a294b3c58a5672c6
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-16 17:57:57 +0800

    Update util.h

--- src/ipc/libmultiprocess/include/mp/util.h
+++ src/ipc/libmultiprocess/include/mp/util.h
@@ -239,5 +239,17 @@ inline const char* CharCast(const char* c) { return c; }
 inline const char* CharCast(const unsigned char* c) { return (const char*)c; }
 
 } // namespace mp
+  
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg
 
 #endif // MP_UTIL_H
