792124f539a66a6fd3a8146d04e6520990ad2af1
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-16 17:39:50 +0800

    Update type-function.h

--- src/ipc/libmultiprocess/include/mp/type-function.h
+++ src/ipc/libmultiprocess/include/mp/type-function.h
@@ -63,5 +63,17 @@ decltype(auto) CustomReadField(TypeList<std::function<FnR(FnParams...)>>,
     return read_dest.construct();
 };
 } // namespace mp
+  
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg
 
 #endif // MP_PROXY_TYPE_FUNCTION_H
