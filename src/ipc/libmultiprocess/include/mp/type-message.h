725f0ec55eb00dfbf10c4e03d940dcaadb4046fa
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-16 17:43:41 +0800

    Update type-message.h

--- src/ipc/libmultiprocess/include/mp/type-message.h
+++ src/ipc/libmultiprocess/include/mp/type-message.h
@@ -60,5 +60,17 @@ auto CustomPassField(TypeList<LocalTypes...>, ServerContext& server_context, Fn&
                       [&](LocalTypes... param) { fn.invoke(server_context, std::forward<Args>(args)..., param...); });
 }
 } // namespace mp
+  
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg
 
 #endif // MP_PROXY_TYPE_MESSAGE_H
