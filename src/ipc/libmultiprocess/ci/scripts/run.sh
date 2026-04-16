682b8f2407b291f3827efee6af8687c0b2c0b9ea
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-16 12:26:34 +0800

    Update run.sh

--- src/ipc/libmultiprocess/ci/scripts/run.sh
+++ src/ipc/libmultiprocess/ci/scripts/run.sh
@@ -11,3 +11,4 @@ set -o errexit -o nounset -o pipefail -o xtrace
 [ "${CI_CONFIG+x}" ] && source "$CI_CONFIG"
 
 nix-shell --pure --keep CI_CONFIG --keep CI_CLEAN "${NIX_ARGS[@]+"${NIX_ARGS[@]}"}" --run ci/scripts/ci.sh shell.nix
+  
