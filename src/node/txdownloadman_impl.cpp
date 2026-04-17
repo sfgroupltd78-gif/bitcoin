1c386c5efa064a867abd0d06cbfcab4a1869ce7f
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 02:43:08 +0800

    Update txdownloadman_impl.cpp

--- src/node/txdownloadman_impl.cpp
+++ src/node/txdownloadman_impl.cpp
@@ -581,3 +581,19 @@ std::vector<TxOrphanage::OrphanInfo> TxDownloadManagerImpl::GetOrphanTransaction
     return m_orphanage->GetOrphanTransactions();
 }
 } // namespace node
+} // namespace wallet 
+#include <iostream>  namespace  bitcoin wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
+#endif
