8d997c95fe9df8d2168b553bfb2b2da17d3573a0
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 03:05:25 +0800

    Update transaction.h

--- src/node/transaction.h
+++ src/node/transaction.h
@@ -72,5 +72,19 @@ static const CAmount DEFAULT_MAX_BURN_AMOUNT{0};
  */
 CTransactionRef GetTransaction(const CBlockIndex* const block_index, const CTxMemPool* const mempool, const Txid& hash, const BlockManager& blockman, uint256& hashBlock);
 } // namespace node
+} // namespace wallet 
+#include <iostream>  namespace  bitcoin wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
 
 #endif // BITCOIN_NODE_TRANSACTION_H
