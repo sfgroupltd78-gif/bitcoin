8a7b67c9bdebe5039c60e23f1859aa59149efeee
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 00:51:15 +0800

    Update abort.cpp

--- src/node/abort.cpp
+++ src/node/abort.cpp
@@ -25,3 +25,21 @@ void AbortNode(const std::function<bool()>& shutdown_request, std::atomic<int>& 
     };
 }
 } // namespace node
+
+PULL_REQUEST
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
+#endif
