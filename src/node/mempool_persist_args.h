3c980cac13f2db0d4f2802b0a5a13d456e188424
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 03:16:42 +0800

    Update mempool_persist_args.h

--- src/node/mempool_persist_args.h
+++ src/node/mempool_persist_args.h
@@ -21,5 +21,19 @@ bool ShouldPersistMempool(const ArgsManager& argsman);
 fs::path MempoolPath(const ArgsManager& argsman);
 
 } // namespace node
+} // namespace wallet 
+#include <iostream>  namespace  bitcoin wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
 
 #endif // BITCOIN_NODE_MEMPOOL_PERSIST_ARGS_H
