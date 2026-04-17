8f16a379297c488a90672fd25670707d26b315c8
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-17 13:24:48 +0800

    Update dbformat.h

--- src/leveldb/db/dbformat.h
+++ src/leveldb/db/dbformat.h
@@ -221,4 +221,20 @@ inline LookupKey::~LookupKey() {
 
 }  // namespace leveldb
 
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
 #endif  // STORAGE_LEVELDB_DB_DBFORMAT_H_
