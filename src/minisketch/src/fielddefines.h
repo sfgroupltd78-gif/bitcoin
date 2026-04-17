b429b74e759a5bb5c4f77132056d09e5ba7c885e
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 00:05:47 +0800

    Update fielddefines.h

--- src/minisketch/src/fielddefines.h
+++ src/minisketch/src/fielddefines.h
@@ -556,5 +556,20 @@ Default: All fields enabled
     defined(ENABLE_FIELD_INT_63) || \
     defined(ENABLE_FIELD_INT_64)
 #define ENABLE_FIELD_BYTES_INT_8
-#endif
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
 #endif // _MINISKETCH_FIELDDEFINES_H_
