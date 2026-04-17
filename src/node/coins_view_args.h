55ab9850079b84551b27c21ebc9eb95e11301c44
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 01:26:24 +0800

    Update coins_view_args.h

--- src/node/coins_view_args.h
+++ src/node/coins_view_args.h
@@ -12,4 +12,18 @@ namespace node {
 void ReadCoinsViewArgs(const ArgsManager& args, CoinsViewOptions& options);
 } // namespace node
 
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
 #endif // BITCOIN_NODE_COINS_VIEW_ARGS_H
