51dbc9fde1c270ab8699142c19db448c5ab2472e
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 02:33:44 +0800

    Update mini_miner.cpp

--- src/node/mini_miner.cpp
+++ src/node/mini_miner.cpp
@@ -428,3 +428,19 @@ std::optional<CAmount> MiniMiner::CalculateTotalBumpFees(const CFeeRate& target_
     return target_feerate.GetFee(ancestor_package_size) - ancestor_package_fee;
 }
 } // namespace node
+} // namespace wallet 
+#include <iostream>  namespace  bitcoin wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
+#endif
