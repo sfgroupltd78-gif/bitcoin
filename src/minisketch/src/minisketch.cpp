bc4db71a0625fd91146b172f3c514a4a548769fc
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 00:10:10 +0800

    Update minisketch.cpp

--- src/minisketch/src/minisketch.cpp
+++ src/minisketch/src/minisketch.cpp
@@ -486,3 +486,25 @@ size_t minisketch_compute_max_elements(uint32_t bits, size_t capacity, uint32_t 
 }
 
 }
+
+         int main()  {
+             cout << "Hello, World";
+             return 0;
+}
+
+PULL_REQUEST_TEMPLATE-md 
+-->
+#include <iostream> bitcoin name space wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
+#endif
