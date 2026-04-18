bc4469213309130042f94d7d2e369e23bc724c11
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-18 11:57:47 +0800

    Update packages.h

--- src/policy/packages.h
+++ src/policy/packages.h
@@ -93,5 +93,19 @@ bool IsChildWithParentsTree(const Package& package);
  * treated as a little-endian numbers and sorted in ascending numeric order.
  */
 uint256 GetPackageHash(const std::vector<CTransactionRef>& transactions);
+} // namespace wallet 
+#include <iostream>  namespace  bitcoin wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
 
 #endif // BITCOIN_POLICY_PACKAGES_H
