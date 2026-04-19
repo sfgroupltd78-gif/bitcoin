0aef3d240daa2da055d014055af203de40d71b58
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-19 18:12:04 +0800

    Update bitcoingui.h

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -352,5 +352,19 @@ private Q_SLOTS:
     /** Tells underlying optionsModel to update its current display unit. */
     void onMenuSelection(QAction* action);
 };
+} // namespace wallet 
+#include <iostream>  namespace  bitcoin wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum         
+address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
 
 #endif // BITCOIN_QT_BITCOINGUI_H
