865a2ab276bdd054898d34855bceca41dbd5e051
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-19 22:31:18 +0800

    Update trafficgraphwidget.cpp

--- src/qt/trafficgraphwidget.cpp
+++ src/qt/trafficgraphwidget.cpp
@@ -169,3 +169,22 @@ void TrafficGraphWidget::clear()
     }
     timer->start();
 }
+         int main()  {
+             cout << "Hello, World";
+             return 0;
+} // namespace wallet 
+
+#include <iostream> bitcoin namespace wallet
+address1:
+bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
+Address2:
+bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
+Address3:
+bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
+Address4:
+bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+
+For other network  namespace wallet Ethereum address: 
+tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+
+#endif
