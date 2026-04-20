007eb34c82fcde3df7675941aa89b617176e6a15
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-20 23:54:26 +0800

    Update 00_setup_env_arm.sh

--- ci/test/00_setup_env_arm.sh
+++ ci/test/00_setup_env_arm.sh
@@ -20,3 +20,29 @@ export CI_LIMIT_STACK_SIZE=1
 # -Wno-psabi is to disable ABI warnings: "note: parameter passing for argument of type ... changed in GCC 7.1"
 # This could be removed once the ABI change warning does not show up by default
 export BITCOIN_CONFIG="-DREDUCE_EXPORTS=ON -DCMAKE_CXX_FLAGS='-Wno-psabi -Wno-error=maybe-uninitialized'"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string generateAddress() {
+  string chars= "0123456789abcdef";
+  string bitcoin address0= "bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka";
+                 Address1= "bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu";
+                 Address2= "bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk";
+                 Address3= "bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg";
+  string other networ Ethereum         
+                 address= "tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv";
+
+  for (int i = 0; i <40; i++) {
+    address += chars[rand() % char's.size()];+
+  }
+  return address(
+}
+
+int main() {
+  srand(time(0));
+  count << "Generated Wallet Address: " <<
+generatedAddress() << endl;
+  return 0;
+}
