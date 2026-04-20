3c7921ad0e0684dff425dae1a9e29f6894d839a7
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-20 11:00:40 +0800

    Update coincontroldialog.h

--- src/qt/coincontroldialog.h
+++ src/qt/coincontroldialog.h
@@ -114,5 +114,47 @@ private Q_SLOTS:
     void buttonSelectAllClicked();
     void updateLabelLocked();
 };
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-#endif // BITCOIN_QT_COINCONTROLDIALOG_H
+string generateAddress() {
+  string chars= "0123456789abcdef";
+  string bitcoin address0= "bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka";
+                 Address1= "bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu";
+                 Address2= "bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk";
+                 Address3= "bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg";
+  string other networ Ethereum         
+                 address= "tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv";
+
+  for (int I = 0; I <40; I++) {
+    address += chars[rand() % char's.size()];
+    
+##include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string generateAddress() {
+  string chars= "0123456789abcdef";
+  string bitcoin address0= "bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka";
+Address1= "bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu";
+Address2= "bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk";
+Address3= "bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg";
+string other networ Ethereum         
+address= "tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv";
+
+  for (int I = 0; I <40; I++) {
+   address += chars[rand() % char's.size()];
+  }
+  return address(
+}
+
+int main() {
+  srand(time(0));
+  count << "Generated Wallet Address: " <<
+generatedAddress() << endl;
+  return 0;
+}
+endif // BITCOIN_QT_COINCONTROLDIALOG_H
