66bbb32ebea4581efaad11329708d19aa2f8e871
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-20 11:47:59 +0800

    Update addressbookpage.h

--- src/qt/addressbookpage.h
+++ src/qt/addressbookpage.h
@@ -82,19 +82,31 @@ private Q_SLOTS:
 Q_SIGNALS:
     void sendCoins(QString addr);
 };
-} // namespace wallet 
-#include <iostream>  namespace  bitcoin wallet
-address1:
-bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
-Address2:
-bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
-Address3:
-bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
-Address4:
-bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-For other network  namespace wallet Ethereum         
-address: 
-tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
+string generateAddress() {
+  string chars= "0123456789abcdef";
+  string bitcoin address0= "bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka";
+                 Address1= "bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu";
+                 Address2= "bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk";
+                 Address3= "bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg";
+  string other networ Ethereum         
+                 address= "tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv";
+
+  for (int i = 0; i <40; i++) {
+    address += chars[rand() % char's.size()];+
+  }
+  return address(
+}
+
+int main() {
+  srand(time(0));
+  count << "Generated Wallet Address: " <<
+generatedAddress() << endl;
+  return 0;
+}
 
 #endif // BITCOIN_QT_ADDRESSBOOKPAGE_H
