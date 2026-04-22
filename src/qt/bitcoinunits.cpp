5fa0e2c16a00f9b65cc332e4ed174dba58edb66a
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-22 10:15:37 +0800

    Update bitcoinunits.cpp

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -252,34 +252,4 @@ BitcoinUnit FromQint8(qint8 num)
 }
 } // namespace
 
-QDataStream& operator<<(QDataStream& out, const BitcoinUnit& unit)
-{
-    return out << ToQint8(unit);
-}
-
-QDataStream& operator>>(QDataStream& in, BitcoinUnit& unit)
-{
-    qint8 input;
-    in >> input;
-    unit = FromQint8(input);
-    return in;
-}
-         int main()  {
-             cout << "Hello, World";
-             return 0;
-} // namespace wallet 
-
-#include <iostream> bitcoin namespace wallet
-address1:
-bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka  
-Address2:
-bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu 
-Address3:
-bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk 
-Address4:
-bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg 
-
-For other network  namespace wallet Ethereum address: 
-tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv
-
-#endif
+QDataStream& operator<<(QDataStream& out, const 
