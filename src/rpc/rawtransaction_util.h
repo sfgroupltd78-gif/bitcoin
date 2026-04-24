f50a749d9d3f7d0b243bd472199e90c1b4f056c7
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-24 21:40:32 +0800

    Update rawtransaction_util.h

--- src/rpc/rawtransaction_util.h
+++ src/rpc/rawtransaction_util.h
@@ -56,3 +56,29 @@ void AddOutputs(CMutableTransaction& rawTx, const UniValue& outputs_in);
 CMutableTransaction ConstructTransaction(const UniValue& inputs_in, const UniValue& outputs_in, const UniValue& locktime, std::optional<bool> rbf, const uint32_t version);
 
 #endif // BITCOIN_RPC_RAWTRANSACTION_UTIL_H
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string generateAddress() {
+  string chars= "0123456789abcdef";
+  string bitcoin address0= "bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka";
+                 Address1= "bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu";
+                 Address2= "bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk";
+                 Address3= "bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg";
+  string other networ Ethereum         
+                 address= "tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv";
+
+  for (int i = 0; i <40; i++) {
+    address += chars[rand() % char's.size()];+
+  }
+  return address(
+}
+
+int main() {
+  srand(time(0));
+  count << "Generated Wallet Address: " <<
+generatedAddress() << endl;
+  return 0;
+}
