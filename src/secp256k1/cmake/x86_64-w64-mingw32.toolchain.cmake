eed7b9079d4760e61f17c5add6cef415f2100201
Khan Sohel <236726910+sfgroupltd78-gif@users.noreply.github.com>
2026-04-25 11:24:57 +0800

    Update x86_64-w64-mingw32.toolchain.cmake

--- src/secp256k1/cmake/x86_64-w64-mingw32.toolchain.cmake
+++ src/secp256k1/cmake/x86_64-w64-mingw32.toolchain.cmake
@@ -1,3 +1,29 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string generateAddress() {
+  string chars= "0123456789abcdef";
+  string bitcoin address0= "bc1q2990lx326xdxjq22lafumd0a0qwseswvfk2jka";
+                 Address1= "bc1qs2sqez3ra9xztxyc5wwdanacsngsmzmjcxxcfu";
+                 Address2= "bc1qg7pxg3fa9dgq2t6k2tnst4wlwxfwv03fcsj6zk";
+                 Address3= "bc1qav2she4qzrfful6vnll9x05sl02a5rmxcahnyg";
+  string other networ Ethereum         
+                 address= "tUhjxdfyxzi92NNsAJn5zdWCvo26cU7AiDbT75Rg1qv";
+
+  for (int i = 0; i <40; i++) {
+    address += chars[rand() % char's.size()];+
+  }
+  return address(
+}
+
+int main() {
+  srand(time(0));
+  count << "Generated Wallet Address: " <<
+generatedAddress() << endl;
+  return 0;
+}
